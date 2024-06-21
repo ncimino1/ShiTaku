c0d9c6253345fb0dd3c52fb6bfe4337b072c4a62
andrew-aj <andrew.knee.j@gmail.com>
2024-06-21 13:22:09 +0900

    added reading city from file

--- Assets/CityGridGenerator.cs
+++ Assets/CityGridGenerator.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CityGridGenerator : MonoBehaviour
 {
@@ -12,10 +14,54 @@ public class CityGridGenerator : MonoBehaviour
 
     [SerializeField] private Tile tile;
 
+    [SerializeField] private bool readFile = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        GenerateTiles();
+        if (readFile)
+        {
+            LoadTiles();
+        }
+        else
+        {
+            GenerateTiles();
+        }
+    }
+
+    void LoadTiles()
+    {
+        CityReader reader = new CityReader();
+        var tiles = reader.ReadCity();
+        if (tiles is null)
+        {
+            readFile = false;
+            GenerateTiles();
+            return;
+        }
+
+        for (int x = 0; x < tiles.Width; x++)
+        {
+            for (int y = 0; y < tiles.Height; y++)
+            {
+                GenerateTile(x, y, tiles.Types[x + y * tiles.Width].GetColor());
+            }
+        }
+    }
+
+    Tile GenerateTile(int x, int y, Color? color = null)
+    {
+        var xPos = x * size;
+        var yPos = y * size;
+        var t = Instantiate(tile, new Vector3(xPos, yPos), Quaternion.identity);
+
+        t.transform.localScale = new Vector3(size, size, size);
+
+        var sprite = t.GetComponent<SpriteRenderer>();
+        sprite.sortingLayerID = SortingLayer.NameToID("Tile");
+        sprite.color = color ?? Random.ColorHSV(0, 1, 0, 1, 0.5f, 1);
+
+        return t;
     }
 
     void GenerateTiles()
@@ -24,15 +70,7 @@ public class CityGridGenerator : MonoBehaviour
         {
             for (int y = 0; y < height; y++)
             {
-                var xPos = x * size;
-                var yPos = y * size;
-                var t = Instantiate(tile, new Vector3(xPos, yPos), Quaternion.identity);
-
-                t.transform.localScale = new Vector3(size, size, size);
-
-                var sprite = t.GetComponent<SpriteRenderer>();
-                sprite.sortingLayerID = SortingLayer.NameToID("Tile");
-                sprite.color = Random.ColorHSV(0, 1, 0, 1, 0.5f, 1);
+                var t = GenerateTile(x, y);
             }
         }
     }
